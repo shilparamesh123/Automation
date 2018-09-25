de1802eb8d7b63d9ff5b3eeac0ce5087e75b85f5
shilparamesh123 <43568251+shilparamesh123@users.noreply.github.com>
2018-09-25 22:00:48 +0530

    gyggjhfjgf

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -10,7 +10,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class DemoA {
 
 	public static void main(String[] args) throws Exception{
-		Workbook w= WorkbookFactory.create(new File("./data/book1.xlsx"));
+		Workbook w= WorkbookFactory.create(new File("./data/book.xlsx"));
         String s=w.getSheet("sheet1").getRow(0).getCell(0).toString();
         System.out.println(s);
 	}
